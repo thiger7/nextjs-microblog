1dddd058a7b6d2b8895249c496ed4a32223f1c48
adf0423 <adf0423@gmail.com>
2023-07-17 00:23:08 +0900

    first commit

---
+++ components/Layout.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
+
+const name = "えいむん";
+export const siteTitle = "Next.js Blog";
+
+function Layout({ children }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico"/>
+      </Head>
+      <header className={styles.header}>
+        <img src="/images/profile.jpeg" className={utilStyles.borderCircle} width="100px" />
+        <h1 className={utilStyles.heading2Xl}>{ name }</h1>
+      </header>
+      <main>{children}</main>
+    </div>
+  );
+}
+
+export default Layout;
